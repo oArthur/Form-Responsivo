bbb2515626fa19836256a97219aa273287b32138
oArthur <arthursouzamaciel@gmail.com>
2021-07-07 14:08:10 -0300

    :up:

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var form = document.getElementById('formulario');
+var username = document.getElementById('username');
+
+
+
+
+
+
+
+
+// form.addEventListener('submit', function(e) {
+//     // alerta o valor do campo
+//     alert(username.value);
+
+//     // impede o envio do form
+//     e.preventDefault();
+// });
